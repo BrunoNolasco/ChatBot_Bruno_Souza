7dcff06870bd0bae66aac38b3878c09553f64b2c
BrunoNolasco <brunonolasco2763@gmail.com>
2025-07-23 10:49:21 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,62 +1,113 @@
 import os
 from datetime import datetime
 
-def obter_resposta(texto: str) -> str:
-    comando: str = texto.lower()
+def chatbot():
+    nome = input("Olá! Qual o seu nome? ")
+    print(f"Prazer em te conhecer, {nome}!")
 
-    if comando in ('olá', 'boa tarde', 'bom dia'):
-        return 'Olá tudo bem!'
-    if comando == 'como estás':
-        return 'Estou bem, obrigado!'
-    if comando == 'como te chamas?':
-        return 'O meu nome é: Bot :)'
-    if comando == 'tempo':
-        return 'Está um dia de sol!'
-    if comando in ('bye', 'adeus', 'tchau'):
-        return 'Gostei de falar contigo! Até breve...'
-    if 'horas' in comando:
-        return f'São: {datetime.now():%H:%M} horas'
-    if 'data' in comando:
-        return f'Hoje é dia: {datetime.now():%d-%m-%Y}'
+    humor = input("Como você está hoje? (bem/mal) ").strip().lower()
+    if humor == "bem":
+        print("Que ótimo! Espero que continue assim!")
+    elif humor == "mal":
+        print("Sinto muito, Espero que o seu dia melhore!")
+    else:
+        print("Não entendi, mas espero que esteja tudo bem!")
 
-    return f'Desculpa, não entendi a questão! {texto}'
-
-    # respostas = {
-    #     ('olá', 'boa tarde', 'bom dia'): 'Olá tudo bem!',
-    #     'como estás': 'Estou bem, obrigado!',
-    #     ('bye', 'adeus', 'tchau'): 'Gostei de falar contigo! Até breve...',
-    # }
-
-    # for chave, resposta in respostas.items():
-    #     if isinstance(chave, tuple):
-    #         if comando in chave:
-    #             return resposta
-    #     elif chave in comando:
-    #         return resposta
-
-    # return f'Desculpa, não entendi a questão! {texto}'
+chatbot()
 
 
-def chat() -> None:
-    print('Bem-vindo ao ChatBot!')
-    print('Escreva "bye" para sair do chat')
-    name: str = input('Bot: Como te chamas? ')
-    print(f'Bot: Olá, {name}! \n Como te posso ajudar?')
-
-    while True:
-        user_input: str = input('Tu: ')
-
-        if resposta == 'Gostei de falar contigo! Até breve...':
-            break
-
-    print('Chat acabou')
-    print()
 
 
-def main() -> None:
-    os.system('cls' if os.name == 'nt' else 'clear')
-    chat()
 
 
-if __name__ == '__main__':
-    main()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#def obter_resposta(texto: str) -> str:
+#    comando: str = texto.lower()
+#
+#                #if comando in ('olá', 'boa tarde', 'bom dia'):
+#                #    return 'Olá tudo bem!'
+#                #if comando == 'como estás':
+#                #    return 'Estou bem, obrigado!'
+#                #if comando == 'como te chamas?':
+#                #    return 'O meu nome é: Bot :)'
+#                #if comando == 'tempo':
+#                #    return 'Está um dia de sol!'
+#                #if comando in ('bye', 'adeus', 'tchau'):
+#                #    return 'Gostei de falar contigo! Até breve...'
+#                #if 'horas' in comando:
+#                #    return f'São: {datetime.now():%H:%M} horas'
+#                #if 'data' in comando:
+#                #    return f'Hoje é dia: {datetime.now():%d-%m-%Y}'
+#           
+#                #return f'Desculpa, não entendi a questão! {texto}'
+#
+#    respostas = {
+#        ('olá', 'boa tarde', 'bom dia'): 'Olá tudo bem!',
+#        'como estás': 'Estou bem, obrigado!',
+#        ('bye', 'adeus', 'tchau'): 'Gostei de falar contigo! Até breve...',}
+#    for chave, resposta in respostas.items():
+#        if isinstance(chave, tuple):
+#            if comando in chave:
+#                return resposta
+#        elif chave in comando:
+#            return resposta
+#        return f'Desculpa, não entendi a questão! {texto}'
+#
+#
+#def chat() -> None:
+#    print('Bem-vindo ao ChatBot!')
+#    print('Escreva "bye" para sair do chat')
+#    name: str = input('Bot: Como te chamas? ')
+#    print(f'Bot: Olá, {name}! \n Como te posso ajudar?')
+#
+#    while True:
+#        user_input: str = input('Tu: ')
+#
+#        if resposta == 'Gostei de falar contigo! Até breve...':
+#            break
+#
+#    print('Chat acabou')
+#    print()
+#
+#
+#def main() -> None:
+#    os.system('cls' if os.name == 'nt' else 'clear')
+#    chat()
+#
+#
+#if __name__ == '__main__':
+#    main()
